91749cb8bab6b79ed662b063a2577d2ece8e9fe0
Marieta909 <112102130+Marieta909@users.noreply.github.com>
2022-09-04 23:22:20 +0400

    Update newserver.js

--- newserver.js
+++ newserver.js
@@ -1,11 +1,27 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 8080;
-const server = http.createServer();
+import express from "express";
+const app = express();
+const port=process.env.port || 8080;
 
-server.on('request', (req, res) => {
-    res.end('Hello world');
+function factorial(num) {
+  if(num==0) {
+    return 1;
+  }
+  else 
+     return num*factorial(num-1)
+}
+
+app.get("/:number", (req,res) => {
+  let num = req.params["number"];
+  let factorial_num=factorial(parseInt(num));
+  console.log(factorial_num)
+  res.send(factorial_num.toString());
+  
 });
+app.listen (port, err  => {
+  if (err) {
+   return Console.log("Error", err);
+  
+  }
 
-server.listen(port, hostname, () => console.log(`server started at http://${hostname}:${port}/`));
-
+  console.log(`Listening on port ${port}`);
+});
