5c63eedbcdbd8be699a7b539f7c79637c5cc876d
Marieta909 <112102130+Marieta909@users.noreply.github.com>
2022-09-27 21:14:42 +0400

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -75,3 +75,82 @@ app.listen(port, err => {
   }
   console.log(`Listening on port ${port}`);
 });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+const express = require("express");
+const app = express();
+const port = process.env.port || 8000;
+
+const HTML_BLANK = "<!DOCTYPE html> <html lang='en'> <head> <meta charset='UTF-8'> <meta http-equiv='X-UA-Compatible' content='IE=edge'> <meta name='viewport' content='width=device-width, initial-scale=1.0'> <title>Oh My God</title> </head><body> OURTABLEHEARE </body></html>"
+
+function rand50() {
+  return Math.floor(Math.random() * 10) & 1;
+}
+function rand75() {
+  return rand50() | rand50();
+}
+
+function shuffle(array) {
+  let currentIndex = array.length, randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+function createTable(/*colorArr,*/ n, m) {
+  let k = 0;
+  let table = "<table border='10' width = 700px height = 700px align='center'>"
+    let arr = []
+    const randNum = Math.floor(Math.random() * (n*m))
+    for (let i = 0; i < randNum; i++) {
+      arr.push(rand75())
+    }
+    const available = (n*m) - randNum;
+    for (let i = 0; i < available; i++) {
+      arr.push(2)
+    }
+
+  for (let i = 0; i < n; i++) {
+    table += "<tr>"
+    for (let j = 0; j < m; j++) {
+      if (arr[k] == 0)
+        table += "<td align='center' colspan='1' style=background-color:blue> </td> "
+      else if (arr[k] == 1)
+        table += "<td align='center' colspan='1' style=background-color:Green> </td> "
+      else
+        table += "<td align='center' colspan='1' style=background-color:Brown> </td> "
+      k++;
+    }
+    table += "</tr>"
+  }
+  table += "</table>"
+  const HTML = HTML_BLANK.replace('OURTABLEHEARE', table)
+  return HTML
+}
+
+app.get("/", (req, res) => {
+  let colors = createTable();
+  shuffle(colors)
+  let board = createTable(/*colors,*/ 10, 13)
+  res.send(board)
+});
+app.listen(port, err => {
+  if (err) {
+    return Console.log("Error", err);
+
+  }
+  console.log(`Listening on port ${port}`);
+});
